8efdb9d9ac84525ac095746d6b60be3d517cd073
Abdulmalik Muhammad <bawasuru@gmail.com>
2023-03-26 00:26:01 +0100

    integrate api call

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,6 +1,48 @@
-import { Box, Button, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Spinner, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
 
 const MainContent = () => {
+  const query = gql`
+    query ($page: Int!, $perPage: Int!) {
+      Page(page: $page, perPage: $perPage) {
+        pageInfo {
+          total
+          perPage
+          currentPage
+          lastPage
+          hasNextPage
+        }
+        media(type: ANIME, format_in: MOVIE, sort: START_DATE, startDate_greater: 20200000, genre_in: "Action") {
+          siteUrl
+          title {
+            native
+          }
+        }
+      }
+    }
+  `;
+
+  const [page, setPage] = useState(1);
+
+  const { loading, error, data } = useQuery(query, {
+    variables: {
+      page: page,
+      perPage: 20,
+    },
+  });
+
+  if (loading) return <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />;
+  if (error) return `ERORR: ${error.message}`;
+
+  const prevPage = () => {
+    setPage((currentPage) => (currentPage -= 1));
+  };
+
+  const nextPage = () => {
+    setPage((currentPage) => (currentPage += 1));
+  };
+
   return (
     <Box w={"94%"} position="absolute" top={"3%"} marginLeft={10}>
       <Box w={"94%"} borderColor="gray.300" borderWidth={1} borderRadius="10">
@@ -13,26 +55,35 @@ const MainContent = () => {
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
-                <Td fontSize={12} margin={0}>
-                  Naruto
-                </Td>
-                <Td>
-                  <a href={"https://naruto.com"} target="_blank" rel="noreferrer">
-                    https://naruto.com
-                  </a>
-                </Td>
-              </Tr>
+              {data?.Page?.media?.map((data, index) => (
+                <Tr key={index}>
+                  <Td fontSize={12} margin={0}>
+                    {data?.title?.native}
+                  </Td>
+                  <Td>
+                    <a href={data?.siteUrl} target="_blank" rel="noreferrer">
+                      {data?.siteUrl}
+                    </a>
+                  </Td>
+                </Tr>
+              ))}
             </Tbody>
           </Table>
         </TableContainer>
       </Box>
 
       <Box display="flex" flexDirection="row" justifyContent="flex-end" w="94%" marginTop="2" gap={4}>
-        <Button colorScheme="blue" disabled p={2} h={8}>
+        <Button
+          colorScheme="blue"
+          onClick={prevPage}
+          disabled
+          p={2}
+          h={8}
+          isDisabled={data.Page.pageInfo.currentPage === 1}
+        >
           Previous
         </Button>
-        <Button colorScheme="blue" p={2} h={8}>
+        <Button colorScheme="blue" p={2} h={8} isDisabled={!data.Page.pageInfo.hasNextPage} onClick={nextPage}>
           Next
         </Button>
       </Box>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://graphql.anilist.co",
+  // uri: "https://anilist.co/graphiql",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
